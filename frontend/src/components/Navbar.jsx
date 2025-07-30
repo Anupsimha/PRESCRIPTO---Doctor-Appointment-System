9fae12f5457d515fca9c1a3f6e93702061b42c39
Anupsimha <anupsimha2@gmail.com>
2025-07-30 12:07:27 +0530

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { assets } from '../assets/assets_frontend/assets.js';
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.logo} alt="" />
+      <ul></ul>
+    </div>
+  )
+}
+
+export default Navbar
