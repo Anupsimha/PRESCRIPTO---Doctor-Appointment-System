898817bb0eaa6fce05fb90403822d9025eb40690
Anupsimha <anupsimha2@gmail.com>
2025-08-04 14:50:31 +0530

    Added Headers and TopDoctors Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
+import Speciality from '../components/Speciality'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
-      
+      <Header></Header>
+      <Speciality></Speciality>
+      <TopDoctors></TopDoctors>
     </div>
   )
 }
