88018ff3c50ca9de1cf41518d5b8788d267787fd
Anupsimha <anupsimha2@gmail.com>
2025-08-20 12:12:54 +0530

    Worked on Login page and myProfile page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,13 +14,31 @@ const Login = () => {
 
   return (
     <form className='min-h-[80vh] flex items-center' onSubmit={onSubmitHandler}>
-      <div>
-        <p>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
         <p>Please {state === 'Sign Up' ? "sign up" : "log in"} to book appointments.</p>
-        <div>
+        {
+          state === "Sign Up" && <div className='w-full'>
           <p>Full Name</p>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="text" value={name} onChange={(e) => setName(e.target.value)} />
         </div>
+        }
+        <div className='w-full'>
+          <p>Email</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button className='bg-blue-500 text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        {
+          state === 'Sign Up' ? (
+            <p className='text-sm text-zinc-500'>Already have an account? <span className='text-blue-500 underline cursor-pointer' onClick={() => setState('Login')}>Login here</span></p>
+          ) : (
+            <p className='text-sm text-zinc-500'>Don't have an account? <span className='text-blue-500 underline cursor-pointer' onClick={() => setState('Sign Up')}>Click here</span></p>
+          )
+        }
       </div>
     </form>
   )
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets_frontend/assets.js'
 
 const MyProfile = () => {
+
+  const [userData , setUserData] = useState({
+    name : "Edward Vincent",
+    image : assets.profile_pic,
+    email : 'richardjameswap@gmail.com',
+    phone : '+1 123 456 789',
+    address : {
+      line1 : "57th Cross, Richmond",
+      line2 : "Cicrlce, Church Road, London"
+    },
+    gender: 'Male',
+    dob: '2000-01-20'
+  })
+
+  const [isEdit , setIsEdit] = useState(false);
+
   return (
     <div>
       
+      <img src={userData.image} alt="" />
+
+      {
+        isEdit ? 
+          <input type='text' value={userData.name} onChange={e => setUserData(prev => ({...prev,name:e.target.value}))}></input> :
+          <p>{userData.name}</p>
+      }
+
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email Id : </p>
+          <p>{userData.email}</p>
+          <p>Phone : </p>
+          {
+            isEdit ? 
+              <input type='text' value={userData.phone} onChange={e => setUserData(prev => ({...prev , phone:e.target.value}))}></input> :
+              <p>{userData.phone}</p>
+          }
+          <p>Address : </p>
+          {
+            isEdit ? 
+              <p>
+                <input onChange={(e) => setUserData(prev => ({ ...prev , address : { ...prev.address , line1 : e.target.value } }))}  value={userData.address.line1} type='text'></input>
+                <br />
+                <input onChange={(e) => setUserData(prev => ({ ...prev , address : { ...prev.address , line2 : e.target.value } }))}  value={userData.address.line2} type='text'></input>
+              </p>
+              : <p>
+                {userData.address.line1}
+                <br />
+                {userData.address.line2}
+              </p>
+          }
+        </div>
+      </div>
+      <div>
+        <p>BASIC INFORMATION</p>
+        <div>
+          <p>Gender : </p>
+          {
+            isEdit ? 
+              <select onChange={(e) => setUserData(prev => ({ ...prev , gender : e.target.value }))} value={userData.gender}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+              :
+              <p>{userData.gender}</p>
+          }
+          <p>Birthday : </p>
+          {
+            isEdit ? 
+              <input type='date' value={userData.dob} onChange={e => setUserData(prev => ({...prev , dob:e.target.value}))}></input> 
+              :
+              <p>{userData.dob}</p>
+          }
+        </div>
+      </div>
+
+      <div>
+        {
+          isEdit ?
+          <button onClick={() => setIsEdit(false)}>Save Information</button>
+          :
+          <button onClick={() => setIsEdit(true)}>Edit</button>
+        }
+      </div>
     </div>
   )
 }
