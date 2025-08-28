4688d6c8853e6680eb9ade2d04983a2a27851d02
Anupsimha <anupsimha2@gmail.com>
2025-08-28 11:10:36 +0530

    Started on backend userModels and doctorModels

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
 // app config
 const app = express();
 const PORT = process.env.PORT || 4000;
+
 
 // middlwares
 app.use(express.json());
@@ -15,6 +19,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await connectDB();
+  await connectCloudinary();
   console.log(`Server is running on port ${PORT}`);
 });
