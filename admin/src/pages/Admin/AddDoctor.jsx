c8d5aca8c17ec1e3f50848a16c0289e5b62fc850
Anupsimha <anupsimha2@gmail.com>
2025-12-17 13:02:13 +0530

    Worked on completing the add route page

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../../assets/assets";
+import { AdminContext } from "../../context/AdminContext";
+import { toast } from "react-toastify";
+import axios from 'axios'
 
 const AddDoctor = () => {
 
@@ -7,22 +10,78 @@ const AddDoctor = () => {
   const [name , setName] = useState('');
   const [email , setEmail] = useState('');
   const [password , setPassword] = useState('');
-  const [experience , setExperience] = useState('');
+  const [experience , setExperience] = useState('1 year');
   const [fees , setFees] = useState('');
   const [about , setAbout] = useState('');
-  const [speciality , setSpeciality] = useState('');
+  const [speciality , setSpeciality] = useState('General Physician');
   const [degree , setDegree] = useState('');
+  const [address1 , setAddress1] = useState('');
+  const [address2 , setAddress2] = useState('');
+
+  const {backendUrl , aToken} = useContext(AdminContext)
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      if(!docImg){
+        return toast.error('Image Not Selected')
+      }
+
+      const fromData = new FormData()
+      fromData.append('image' , docImg)
+      fromData.append('name' , name)
+      fromData.append('email' , email)
+      fromData.append('password' , password)
+      fromData.append('experience' , experience)
+      fromData.append('fees' , fees)
+      fromData.append('about' , about)
+      fromData.append('speciality' , speciality)
+      fromData.append('degree' , degree)
+      fromData.append('address' , JSON.stringify({line1 : address1, line2: address2}))
+
+      fromData.forEach((value , key) => {
+        console.log(`${key} : ${value}`);
+      });
+
+      const {data} = await axios.post(backendUrl + '/api/admin/add-doctor' , fromData , {
+        headers:{
+          atoken: aToken
+        }
+      })
+
+      if(data.success){
+        toast.success(data.message)
+        setDocImg(false);
+        setName('');
+        setEmail('');
+        setPassword('');
+        setFees('');
+        setAbout('');
+        setDegree('');
+        setAddress1('');
+        setAddress2('');
+      }
+      else{
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+      console.log("Error in adding doctor frontend", error);
+    }
+  }
 
   return (
-    <form className="m-5 w-full">
+    <form onSubmit={onSubmitHandler} className="m-5 w-full">
       <p className="mb-3 text-lg font-medium">Add Doctor</p>
 
       <div className="bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll">
         <div className="flex items-center gap-4 mb-8 text-gray-500">
           <label htmlFor='doc-img'>
-            <img className="w-16 bg-gray-100 rounded-full cursor-pointer" src={assets.upload_area} alt='' />
+            <img className="w-16 bg-gray-100 rounded-full cursor-pointer" src={docImg ? URL.createObjectURL(docImg) : assets.upload_area} alt='' />
           </label>
-          <input type='file' id='doc-img' hidden />
+          <input onChange={(e) => setDocImg(e.target.files[0])} type='file' id='doc-img' hidden />
           <p>
             Upload Doctor <br /> picture
           </p>
@@ -32,22 +91,22 @@ const AddDoctor = () => {
           <div className="w-full lg:flex-1 flex flex-col gap-4">
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Name</p>
-              <input className="border rounded px-3 py-2" type='text' placeholder='Name' required />
+              <input onChange={(e) => setName(e.target.value)} value={name} className="border rounded px-3 py-2" type='text' placeholder='Name' required />
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Email</p>
-              <input className="border rounded px-3 py-2" type='email' placeholder='Email' required />
+              <input onChange={(e) => setEmail(e.target.value)} value={email} className="border rounded px-3 py-2" type='email' placeholder='Email' required />
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Password</p>
-              <input className="border rounded px-3 py-2" type='password' placeholder='Password' required />
+              <input onChange={(e) => setPassword(e.target.value)} value={password} className="border rounded px-3 py-2" type='password' placeholder='Password' required />
             </div>
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Experience</p>
-              <select className="border rounded px-3 py-2" name='' id=''>
+              <select onChange={(e) => setExperience(e.target.value)} value={experience} className="border rounded px-3 py-2" name=''>
                 <option value='1 year'>1 year</option>
                 <option value='2 year'>2 year</option>
                 <option value='3 year'>3 year</option>
@@ -63,14 +122,14 @@ const AddDoctor = () => {
 
             <div className="flex-1 flex flex-col gap-1">
               <p>Fees</p>
-              <input className="border rounded px-3 py-2" type='number' placeholder='Fees' required />
+              <input onChange={(e) => setFees(e.target.value)} value={fees} className="border rounded px-3 py-2" type='number' placeholder='Fees' required />
             </div>
           </div>
 
           <div className="w-full lg:flex-1 flex flex-col gap-4">
             <div className="flex-1 flex flex-col gap-1">
               <p>Speciality</p>
-              <select className="border rounded px-3 py-2" name='' id=''>
+              <select onChange={(e) => setSpeciality(e.target.value)} value={speciality} className="border rounded px-3 py-2" name=''>
                 <option value='General Physician'>General Physician</option>
                 <option value='Gynecologist'>Gynecologist</option>
                 <option value='Dermatologist'>Dermatologist</option>
@@ -81,13 +140,13 @@ const AddDoctor = () => {
 
               <div className="flex-1 flex flex-col gap-1">
                 <p>Education</p>
-                <input className="border rounded px-3 py-2" type='text' placeholder='Education' required />
+                <input onChange={(e) => setDegree(e.target.value)} value={degree} className="border rounded px-3 py-2" type='text' placeholder='Education' required />
               </div>
 
               <div className="flex-1 flex flex-col gap-1">
                 <p>Address</p>
-                <input className="border rounded px-3 py-2" type='text' placeholder='Address 1' required />
-                <input className="border rounded px-3 py-2" type='text' placeholder='Address 2' required />
+                <input onChange={(e) => setAddress1(e.target.value)} value={address1} className="border rounded px-3 py-2" type='text' placeholder='Address 1' required />
+                <input onChange={(e) => setAddress2(e.target.value)} value={address2} className="border rounded px-3 py-2" type='text' placeholder='Address 2' required />
               </div>
 
             </div>
@@ -96,10 +155,10 @@ const AddDoctor = () => {
 
         <div>
           <p className="mt-4 mb-2">About Doctor</p>
-          <textarea className="w-full px-4 pt-2 border rounded" placeholder="Write about Doctor" rows={5} required></textarea>
+          <textarea onChange={(e) => setAbout(e.target.value)} value={about} className="w-full px-4 pt-2 border rounded" placeholder="Write about Doctor" rows={5} required></textarea>
         </div>
 
-        <button className="bg-[#5F6FFF] px-10 py-3 mt-4 text-white rounded-full cursor-pointer">Add doctor</button>
+        <button type="submit" className="bg-[#5F6FFF] px-10 py-3 mt-4 text-white rounded-full cursor-pointer">Add doctor</button>
       </div>
     </form>
   );
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -6,57 +6,93 @@ import jwt from 'jsonwebtoken';
 import Doctor from '../models/doctorModel.js';
 
 // API for adding doctor
-const addDoctor = async (req , res) => {
-    try {
-        const {name , email , password , speciality , degree , experience , about , fees , address} = req.body;
-        const imageFile = req.file;
+const addDoctor = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address
+    } = req.body;
 
-        // checking for all data to add doctor
-        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address){
-            return res.status(400).json({message : "All fields are required"});
-        }
+    const imageFile = req.file;
 
-        // validating email format
-        if(!validator.isEmail(email)){
-            return res.status(400).json({message : "Invalid email format"});
-        }
-
-        // validating strong password
-        if(password.length < 8){
-            return res.status(400).json({message : "Password must be at least 8 characters long"});
-        }
-
-        // hashing doctor password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password , salt);
-
-        // uploading image to cloudinary
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path , {resource_type : "image"});
-        const imageUrl = imageUpload.secure_url;
-
-        const doctorData = {
-            name,
-            email,
-            password : hashedPassword,
-            speciality,
-            degree,
-            experience,
-            about,
-            fees,
-            address,
-            date: Date.now()
-        }
-
-        const newDoctor = new Doctor(doctorData);
-        await newDoctor.save();
-
-        res.json({message : "Doctor added successfully"});
-
-    } catch (error) {
-        console.log("Error in adding doctor" , error);  
-        return res.status(500).json({message : "Internal server error"});    
+    if (
+      !name || !email || !password || !speciality ||
+      !degree || !experience || !about || !fees || !address
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required"
+      });
     }
-}
+
+    if (!imageFile) {
+      return res.status(400).json({
+        success: false,
+        message: "Doctor image is required"
+      });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email format"
+      });
+    }
+
+    if (password.length < 8) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 8 characters long"
+      });
+    }
+
+    const parsedAddress = JSON.parse(address);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const imageUpload = await cloudinary.uploader.upload(
+      imageFile.path,
+      { resource_type: "image" }
+    );
+
+    const doctorData = {
+      name,
+      email,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: parsedAddress,
+      image: imageUpload.secure_url,
+      date: Date.now()
+    };
+
+    const newDoctor = new Doctor(doctorData);
+    await newDoctor.save();
+
+    res.json({
+      success: true,
+      message: "Doctor added successfully"
+    });
+
+  } catch (error) {
+    console.log("Error in adding doctor", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+};
 
 // API For admin login
 const loginAdmin = async (req , res) => {
