0b170db61ce3646607ed7cab31cf00d7c7e4e23d
Anupsimha <anupsimha2@gmail.com>
2025-12-23 10:15:04 +0530

    Worked on Appointments Section

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -183,4 +183,55 @@ const bookAppointment = async (req , res) => {
 }
 
 
-export {registerUser , loginUser , getProfile , updateProfile , bookAppointment};
+// API to get User Appointments
+const listAppointment = async (req , res) => {
+    try {
+        
+        const userId = req.userId
+        const appointments = await appointmentModel.find({userId})
+
+        res.json({ success: true, appointments })
+
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+
+// API to cancel Appointment
+const cancelAppointment = async (req , res) => {
+    try {
+        
+        const userId = req.userId
+        const { appointmentId } = req.body
+
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        // verify Appointment User
+        if(appointmentData.userId !== userId){
+            return res.json({ success : false , message : 'You are not authorized to cancel this appointment' })
+        }
+
+        await appointmentModel.findByIdAndUpdate(appointmentId , {cancelled : true})
+
+        // Releasing Doctor Slot
+        const {docId, slotDate, slotTime} = appointmentData
+
+        const doctorData = await doctorModel.findById(docId)
+
+        let slots_booked = doctorData.slots_booked
+
+        slots_booked[slotDate] = slots_booked[slotDate].filter(e => e !== slotTime)
+
+        await doctorModel.findByIdAndUpdate(docId , {slots_booked})
+
+        res.json({ success: true, message: "Appointment cancelled successfully" })
+
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export {registerUser , loginUser , getProfile , updateProfile , bookAppointment , listAppointment , cancelAppointment};
